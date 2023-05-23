3f977e62c406dd25737842bbc9c2e0ccdb98e947
Руслан Ключенко <klyuchenkorus@gmail.com>
2023-05-23 12:23:59 +0300

    refactor: split methods

--- task3/Program.cs
+++ task3/Program.cs
@@ -12,84 +12,139 @@ internal class Program {
         var counter = 1;
         var moveMap = args.ToDictionary(move => counter++);
 
-        IWinnerService winnerService = new WinnerService(args);
-        IWinnerTableService winnerTableService = new WinnerTableService(winnerService, args);
-        IKeyGenerator keyGenerator = new KeyGenerator();
+        var winnerService = new WinnerService(args);
+        var winnerTableService = new WinnerTableService(winnerService, args);
+        var keyGenerator = new KeyGenerator();
         var rng = new Random();
 
-        while(true) {
-            //Console.Clear();
-
-            var computerMove = args[rng.Next(args.Length)];
-            var key = keyGenerator.GenerateKey();
-            var hmac = keyGenerator.GenerateHmac(key, computerMove);
-
-            Console.WriteLine($"HMAC - {Convert.ToHexString(hmac)}");
-            Console.WriteLine("Available moves:");
-
-            foreach(var move in moveMap) {
-                Console.WriteLine($"{move.Key} - {move.Value}");
-                counter++;
-            }
-            Console.WriteLine("0 - exit");
-            Console.WriteLine("? - help");
-            Console.Write("Enter your move: ");
-
-            var input = Console.ReadLine();
-            if (input == "?") {
-                Console.WriteLine(winnerTableService.GetWinnerTable());
-                continue;
-            }
-
-            if(!int.TryParse(input, out var choice)) continue;
-
-            if (choice == 0) break;
-
-            if (!moveMap.ContainsKey(choice)) continue;
-
-            var userMove = moveMap[choice];
-            Console.WriteLine($"Your move: {userMove}");
-            Console.WriteLine($"Computer move: {computerMove}");
-
-            var result = winnerService.GetWinner(userMove, computerMove);
-            Console.WriteLine(result < 1 ? 
-                result == 0 ? "Draw!" : "You lose!"
-                : "You win!");
-            Console.WriteLine($"HMAC key: {Convert.ToHexString(key)}");
-            Console.WriteLine();
-            Console.WriteLine();
-        }
-
+        while(_RunMatch(args, moveMap, rng, winnerService, winnerTableService, keyGenerator)) {}
     }
 
+    private static bool _RunMatch(
+                            string[] moves,
+                            Dictionary<int, string> moveMap,
+                            Random rng,
+                            IWinnerService winnerService,
+                            IWinnerTableService winnerTableService,
+                            IKeyGenerator keyGenerator) {
+
+        var computerMove = moves[rng.Next(moves.Length)];
+        var key = keyGenerator.GenerateKey();
+        var hmac = keyGenerator.GenerateHmac(key, computerMove);
+
+        _PrintMenu(moveMap, hmac);
+
+        var userMoveIndex = _GetUserMove(winnerTableService, out var isExitRequired);
+
+        if(isExitRequired)
+            return false;
+
+        if(!moveMap.ContainsKey(userMoveIndex))
+            return true;
+
+        var userMove = moveMap[userMoveIndex];
+
+        _PrintMatchResult(userMove, computerMove, key, winnerService);
+        return true;
+    }
+
+    #region UserInteraction
+
+    private static void _PrintMatchResult(string userMove, string computerMove, byte[] key, IWinnerService winnerService) {
+        Console.WriteLine($"Your move: {userMove}");
+        Console.WriteLine($"Computer move: {computerMove}");
+
+        var result = winnerService.GetWinner(userMove, computerMove);
+        Console.WriteLine(result < 1 ?
+            result == 0 ? "Draw!" : "You lose!"
+            : "You win!");
+        Console.WriteLine($"HMAC key: {Convert.ToHexString(key)}");
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+
+    private static void _PrintMenu(Dictionary<int, string> moveMap, byte[] hmac) {
+        Console.WriteLine($"HMAC - {Convert.ToHexString(hmac)}");
+        Console.WriteLine("Available moves:");
+
+        foreach(var move in moveMap) {
+            Console.WriteLine($"{move.Key} - {move.Value}");
+        }
+        Console.WriteLine("0 - exit");
+        Console.WriteLine("? - help");
+        Console.Write("Enter your move: ");
+    }
+
+    private static int _GetUserMove(IWinnerTableService winnerTableService, out bool isExitRequired) {
+        isExitRequired = false;
+
+        var input = Console.ReadLine();
+        if(input == "?") {
+            Console.WriteLine(winnerTableService.GetWinnerTable());
+            return -1;
+        }
+
+        if(!int.TryParse(input, out var choice))
+            return -1;
+
+        if(choice == 0) {
+            isExitRequired = true;
+            return -1;
+        }
+
+        return choice;
+    }
+
+    #endregion
+
+    #region Validation
+
     private static bool _ValidateInputArgs(string[] args) {
-        if (args.Length < 3) {
+        if(!_ValidateAtLeastThreeMoves(args)) return false;
+
+        if(!_ValidateOddNumberOfMoves(args)) return false;
+
+        if (!_ValidateNoDuplicateMoves(args)) return false;
+
+        return true;
+    }
+
+    private static bool _ValidateAtLeastThreeMoves(string[] moves) {
+        if(moves.Length < 3) {
             Console.WriteLine("Should be at least 3 moves available!");
             _ShowExample();
             return false;
         }
+        return true;
+    }
 
-        if (args.Length % 2 == 0) {
+    private static bool _ValidateOddNumberOfMoves(string[] moves) {
+        if(moves.Length % 2 == 0) {
             Console.WriteLine("Should be an odd number of moves available!");
             _ShowExample();
             return false;
         }
+        return true;
+    }
 
+    private static bool _ValidateNoDuplicateMoves(string[] moves) {
         var set = new HashSet<string>();
-        foreach(var arg in args) {
-            if (!set.Add(arg)) {
+        foreach(var move in moves) {
+            if(!set.Add(move)) {
                 Console.WriteLine("Available moves could not be duplicate!");
                 _ShowExample();
                 return false;
             }
         }
-
         return true;
     }
 
     private static void _ShowExample() {
         Console.WriteLine("Example: rock paper scissors");
     }
+
+    #endregion
+
 }
 
 
--- task3/Services/WinnerTableService.cs
+++ task3/Services/WinnerTableService.cs
@@ -19,33 +19,33 @@ namespace task3.Services
             _moves = moves as List<string> ?? moves.ToList();
         }
 
-        public string GetWinnerTable()
-        {
+        public string GetWinnerTable() {
             var sb = new StringBuilder();
+            var align = _GetTableAlignment();
+
+            _BuildTableHead(sb, align);
+            _BuildTableBody(sb, align);
+
+            return sb.ToString();
+        }
+
+        private int _GetTableAlignment() {
             var align = 0;
 
-            foreach (var move in _moves)
-            {
-                if (align < move.Length)
+            foreach(var move in _moves) {
+                if(align < move.Length)
                     align = move.Length;
             }
 
-            sb.AppendFormat("  {0, align} ".Replace("align", align.ToString()), "");
+            return align;
+        }
 
-            foreach (var move in _moves)
-            {
-                sb.Append(_GetFormattedString(move, align));
-            }
-
-            sb.AppendLine("|");
-
+        private void _BuildTableBody(StringBuilder sb, int align) {
             var counter = 0;
-            while (counter < _moves.Count)
-            {
+            while(counter < _moves.Count) {
                 sb.Append(_GetFormattedString(_moves[counter], align));
 
-                foreach (var move in _moves)
-                {
+                foreach(var move in _moves) {
                     var result = _winnerService.GetWinner(_moves[counter], move);
                     sb.Append(_GetFormattedString(
                         result < 1 ?
@@ -57,8 +57,16 @@ namespace task3.Services
                 sb.AppendLine("|");
                 counter++;
             }
+        }
 
-            return sb.ToString();
+        private void _BuildTableHead(StringBuilder sb, int align) {
+            sb.AppendFormat("  {0, align} ".Replace("align", align.ToString()), "");
+
+            foreach(var move in _moves) {
+                sb.Append(_GetFormattedString(move, align));
+            }
+
+            sb.AppendLine("|");
         }
 
         private string _GetFormattedString(string value, int align) => string.Format("| {0, -align} ".Replace("align", align.ToString()), value);
